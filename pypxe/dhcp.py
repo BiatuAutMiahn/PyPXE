BiatuAutMiahn/PyPXE

commit 317d02d9449db819083bb97b6bec202953690fb7

Added exporting leases to json. Fixes #71

pypxe/dhcp.py
@@ -8,12 +8,13 @@ import socket
 import struct
 import os
 import logging
+import signal
+import json
 from collections import defaultdict
 from time import time
 
 class OutOfLeasesError(Exception):
     pass
-
 
 class DHCPD:
     '''
@@ -47,6 +48,7 @@ class DHCPD:
         self.mode_verbose = server_settings.get('mode_verbose', False) # debug mode
         self.mode_debug = server_settings.get('mode_debug', False) # debug mode
         self.logger = server_settings.get('logger', None)
+        self.saveleasesfile = server_settings.get('saveleases', '')
         self.magic = struct.pack('!I', 0x63825363) # magic cookie
 
         # setup logger
@@ -98,7 +100,39 @@ class DHCPD:
         self.sock.bind(('', self.port ))
 
         # key is MAC
+        # separate options dict so we don't have to clean up on export
+        self.options = dict()
         self.leases = defaultdict(lambda: {'ip': '', 'expire': 0, 'ipxe': self.ipxe})
+        if self.saveleasesfile:
+            try:
+                leasesfile = open(self.saveleasesfile, 'rb')
+                imported = json.load(leasesfile)
+                importsafe = dict()
+                for lease in imported:
+                    packedmac = struct.pack('BBBBBB', *map(lambda x:int(x, 16), lease.split(':')))
+                    importsafe[packedmac] = imported[lease]
+                self.leases.update(importsafe)
+                self.logger.info('Loaded leases from {0}'.format(self.saveleasesfile))
+            except IOError, ValueError:
+                pass
+
+        signal.signal(signal.SIGINT, self.exportleases)
+        signal.signal(signal.SIGTERM, self.exportleases)
+        signal.signal(signal.SIGALRM, self.exportleases)
+        signal.signal(signal.SIGHUP, self.exportleases)
+
+    def exportleases(self, signum, frame):
+        if self.saveleasesfile:
+            exportsafe = dict()
+            for lease in self.leases:
+                # translate the key to json safe (and human readable) mac
+                exportsafe[self.get_mac(lease)] = self.leases[lease]
+            leasesfile = open(self.saveleasesfile, 'wb')
+            json.dump(exportsafe, leasesfile)
+            self.logger.info('Exported leases to {0}'.format(self.saveleasesfile))
+        # if ^C, propagate upwards
+        if signum == signal.SIGINT:
+            raise KeyboardInterrupt
 
     def get_namespaced_static(self, path, fallback = {}):
         statics = self.static_config
@@ -292,7 +326,7 @@ class DHCPD:
         if self.whitelist and self.get_mac(client_mac) not in self.get_namespaced_static('dhcp.binding'):
             self.logger.info('Non-whitelisted client request received from {0}'.format(self.get_mac(client_mac)))
             return False
-        if 60 in self.leases[client_mac]['options'] and 'PXEClient' in self.leases[client_mac]['options'][60][0]:
+        if 60 in self.options[client_mac] and 'PXEClient' in self.options[client_mac][60][0]:
             self.logger.info('PXE client request received from {0}'.format(self.get_mac(client_mac)))
             return True
         self.logger.info('Non-PXE client request received from {0}'.format(self.get_mac(client_mac)))
@@ -307,14 +341,14 @@ class DHCPD:
             self.logger.debug('<--BEGIN MESSAGE-->')
             self.logger.debug('{0}'.format(repr(message)))
             self.logger.debug('<--END MESSAGE-->')
-            self.leases[client_mac]['options'] = self.tlv_parse(message[240:])
+            self.options[client_mac] = self.tlv_parse(message[240:])
             self.logger.debug('Parsed received options')
             self.logger.debug('<--BEGIN OPTIONS-->')
-            self.logger.debug('{0}'.format(repr(self.leases[client_mac]['options'])))
+            self.logger.debug('{0}'.format(repr(self.options[client_mac])))
             self.logger.debug('<--END OPTIONS-->')
             if not self.validate_req(client_mac):
                 continue
-            type = ord(self.leases[client_mac]['options'][53][0]) # see RFC2131, page 10
+            type = ord(self.options[client_mac][53][0]) # see RFC2131, page 10
             if type == 1:
                 self.logger.debug('Received DHCPOFFER from {0}'.format(self.get_mac(client_mac)))
                 try:

pypxe/server.py
@@ -67,6 +67,7 @@ def parse_cli_arguments():
     parser.add_argument('--verbose', action = 'store', dest = 'MODE_VERBOSE', help = 'Comma Seperated (http,tftp,dhcp). Adds verbosity to the selected services while they run. Less verbose than \'debug\'. Use \'all\' for enabling verbosity on all services. Precede an option with \'-\' to disable debugging for that service; as an example, one can pass in the following to enable debugging for all services except the DHCP service: \'--debug all,-dhcp\'', default = SETTINGS['MODE_VERBOSE'])
     parser.add_argument('--config', action = 'store', dest = 'JSON_CONFIG', help = 'Configure from a JSON file rather than the command line', default = '')
     parser.add_argument('--static-config', action = 'store', dest = 'STATIC_CONFIG', help = 'Configure leases from a json file rather than the command line', default = '')
+    parser.add_argument('--saveleases', action = 'store', dest = 'LEASES_FILE', help = 'Save all DHCP leases on exit or SIGHUP. Will load from this file on start', default = '')
     parser.add_argument('--syslog', action = 'store', dest = 'SYSLOG_SERVER', help = 'Syslog server', default = SETTINGS['SYSLOG_SERVER'])
     parser.add_argument('--syslog-port', action = 'store', dest = 'SYSLOG_PORT', help = 'Syslog server port', default = SETTINGS['SYSLOG_PORT'])
 
@@ -236,7 +237,8 @@ def main():
                 mode_verbose = do_verbose('dhcp'),
                 whitelist = args.DHCP_WHITELIST,
                 static_config = loaded_statics,
-                logger = dhcp_logger)
+                logger = dhcp_logger,
+                saveleases = args.LEASES_FILE)
             dhcpd = threading.Thread(target = dhcp_server.listen)
             dhcpd.daemon = True
             dhcpd.start()

commit 1b98c6f64ebbb4583a0017ada28ffefec7563075

dhcp: find the options in self.options, not the client's lease
Up until 317d02d9449d, the DHCP options for a client were found in the
client lease. This however was changed in favor of using a dictionary
instead.

I agree with the change, since I don't see why we should store options
in the lease, but it failed to update the code that was selecting the
best filename per architecture.

This code was further-disabled by 0d1b0d5d2a2f ("pypxe/dhcp.py: avoid
exception when leases have no options"), by not crashing when the
code would have been used.

Let's fix the original regression properly, until we propose a new
way of storing the preferred file names.

Fixes: 317d02d9449d ("Added exporting leases to json. Fixes #71")

pypxe/dhcp.py
@@ -182,8 +182,8 @@ class DHCPD:
         if not filename:
             if not self.ipxe or not self.leases[client_mac]['ipxe']:
                 # http://www.syslinux.org/wiki/index.php/PXELINUX#UEFI
-                if 'options' in self.leases[client_mac] and 93 in self.leases[client_mac]['options'] and not self.file_name:
-                    [arch] = struct.unpack("!H", self.leases[client_mac]['options'][93][0])
+                if 93 in self.options and not self.file_name:
+                    [arch] = struct.unpack("!H", self.options[93][0])
                     filename = {0: 'pxelinux.0', # BIOS/default
                                 6: 'syslinux.efi32', # EFI IA32
                                 7: 'syslinux.efi64', # EFI BC, x86-64